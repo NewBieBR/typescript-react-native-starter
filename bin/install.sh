db35c8d2913f7ebdbd9a6589b7163332554d965c
Hung Dao <hung.dao-nguyen@epitech.eu>
2019-07-05 23:17:43 +0200

    testing install script

--- bin/install.sh
+++ bin/install.sh
@@ -2,14 +2,9 @@
 
 if [[ $# -ge 1 ]]
 then
-    defaultAppName="TypescriptReactNative"
-    defaultPackageName="typescript-react-native"
     projectName="$1"
-    sed -i "" "s/${defaultAppName}/${projectName}/g" app.json
-    sed -i "" "s/${defaultPackageName}/${projectName}/g" package.json
-    rm -rf ios android
+    yarn run rename $projectName
     yarn
-    react-native eject
     react-native link
     rm -rf .git
     echo -e "\033[33m> [ Remember to change the Codecov token in package.json > scripts > codecov ] \033[0m"
