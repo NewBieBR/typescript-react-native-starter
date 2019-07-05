2bd90d4cadee637dcfb7006c0742f02feaaaced7
Hung Dao <hung.dao-nguyen@epitech.eu>
2019-07-05 23:56:19 +0200

    testing install script

--- bin/install.sh
+++ bin/install.sh
@@ -7,8 +7,7 @@ then
     projectName="$1"
     yarn
     yarn run rename $projectName
-    sed -i "" "s/${defaultAppName}/${projectName}/g" app.json
-    sed -i "" "s/${defaultPackageName}/${projectName}/g" package.json
+    watchman watch-del-all
     rm -rf .git
     echo -e "\033[33m> [ Remember to change the Codecov token in package.json > scripts > codecov ] \033[0m"
 else
