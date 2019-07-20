ffc7786c22bc40ba402295fe713d48f09394bffd
Hung Dao <hung.dao-nguyen@epitech.eu>
2019-07-20 15:29:30 +0200

    test codecov

--- src/containers/Settings.tsx
+++ src/containers/Settings.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class Settings extends React.PureComponent {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Text> Settings </Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
