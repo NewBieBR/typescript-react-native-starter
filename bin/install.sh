1740b93a93b99e07d6a100372c16059765c07aa1
Hung Dao <hung.dao-nguyen@epitech.eu>
2019-07-27 14:59:27 +0200

    update installation script

--- bin/install.sh
+++ bin/install.sh
@@ -2,6 +2,7 @@
 YUM_CMD=$(which yum)
 APT_GET_CMD=$(which apt-get)
 BREW_CMD=$(which brew)
+IMAGEMAGICK_CMD=$(which imagemagick)
 if [[ $# -ge 1 ]]
 then
     projectName="$1"
@@ -10,15 +11,17 @@ then
     yarn rename $projectName
     yarn jetify
     cd ios && pod install
-    if [[ ! -z $YUM_CMD ]]; then
-        sudo yum install imagemagick
-    elif [[ ! -z $APT_GET_CMD ]]; then
-        sudo apt-get install imagemagick
-    elif [[ ! -z $BREW_CMD ]]; then
-        brew install imagemagick
-    else
-        echo "Can't install package imagemagick, you should install it yourself in order to use the app-icon generator"
-        exit 1;
+    if [[ ! -z $IMAGEMAGICK_CMD ]]; then
+        if [[ ! -z $YUM_CMD ]]; then
+            sudo yum install imagemagick
+            elif [[ ! -z $APT_GET_CMD ]]; then
+            sudo apt-get install imagemagick
+            elif [[ ! -z $BREW_CMD ]]; then
+            brew install imagemagick
+        else
+            echo "Can't install package imagemagick, you should install it yourself in order to use the app-icon generator"
+            exit 1;
+        fi
     fi
     echo -e "\033[33m> [ Remember to change the Codecov token in package.json > scripts > codecov ] \033[0m"
 else
