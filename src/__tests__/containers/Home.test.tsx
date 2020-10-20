ceeceded61b70029fb97c82814ff18761fcf8a22
Arthur Dao <hung.dao-nguyen@epitech.eu>
2020-10-20 16:05:50 +0200

    :white_check_mark: (Home) remove temporary fetch user test

--- src/__tests__/containers/Home.test.tsx
+++ src/__tests__/containers/Home.test.tsx
@@ -1,12 +1,9 @@
+import {render} from '@testing-library/react-native';
 import React from 'react';
-import {fireEvent, render, waitFor} from '@testing-library/react-native';
-import Home from 'src/containers/Home';
+import 'react-native';
 import {Provider} from 'react-redux';
+import Home from 'src/containers/Home';
 import {store} from 'src/store';
-import nock from 'nock';
-import JSONPlacholderAPI from 'src/lib/jsonPlaceholderAPI';
-
-const mockFetchUser = jest.fn();
 
 describe('Home', () => {
   const pageTree = (
@@ -17,20 +14,5 @@ describe('Home', () => {
   it('should renders correctly', () => {
     const page = render(pageTree);
     expect(page.toJSON()).toMatchSnapshot();
-  });
-
-  it('should fetch user on click', async () => {
-    nock(`${JSONPlacholderAPI.apiEntry}`)
-      .get(`/${JSONPlacholderAPI.usersNamespace}/1`)
-      .reply(200, {
-        body: {id: '1', email: '1', name: '1', phone: '1'},
-      });
-    const page = render(pageTree);
-    const fetchUserButton = page.getByText('fetchUser');
-    fireEvent.press(fetchUserButton);
-    await waitFor(() => {
-      expect(mockFetchUser).toBeCalledTimes(1);
-    });
-    page.debug();
   });
 });
